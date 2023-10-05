ae5c5bcbc2e612c278a683daf95549aa7ef6e09c
Sanchez, Edrian Joel G <146083695+ejsanchez0912@users.noreply.github.com>
2023-10-05 21:02:20 +0800

    Add files via upload

---
+++ act2.js
@@ -0,0 +1,26 @@
+let firstFavNumb = 20, secondFavNumb = 69;
+
+if (firstFavNumb > secondFavNumb){
+    console.log("firstFavNumb is greater than secondFavNumb")
+}
+else if (firstFavNumb < secondFavNumb){
+    console.log("firstFavNumb is less than secondFavNumb")
+}
+else if (firstFavNumb >= secondFavNumb){
+    console.log("firstFavNumb is greater than & equal to secondFavNumb")
+}
+else if (firstFavNumb <= secondFavNumb){
+    console.log("firstFavNumb is less than & equal to secondFavNumb")
+}
+else if (firstFavNumb === secondFavNumb){
+    console.log("firstFavNumb is equal to secondFavNumb")
+}
+else if (firstFavNumb == secondFavNumb){
+    console.log("firstFavNumb is equal to secondFavNumb")
+}
+else if (firstFavNumb !== secondFavNumb){
+    console.log("firstFavNumb is not equal to secondFavNumb")
+}
+else (firstFavNumb != secondFavNumb){
+    console.log("firstFavNumb is not equal to secondFavNumb")
+}
