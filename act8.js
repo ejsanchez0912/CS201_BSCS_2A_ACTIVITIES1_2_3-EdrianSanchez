2011f7695f27c684d3c948acd09a090c3aa1564d
Sanchez, Edrian Joel G <146083695+ejsanchez0912@users.noreply.github.com>
2023-10-14 07:38:47 +0800

    Update act8.js

---
+++ act8.js
@@ -0,0 +1,20 @@
+const favSingers = [ "Andrew E.","Francis Magalona","Bamboo Mañalac"];
+
+console.log(favSingers[1]);
+console.log(favSingers[2]);
+console.log(favSingers[3]);
+
+var favNumbers = ['09', '72', '2001', '0270'];
+
+console.log(favNumbers[1]);
+console.log(favNumbers[2]);
+console.log(favNumbers[3]);
+console.log(favNumbers[4]);
+
+let mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log(favSingers[1]);
+console.log(favSingers[2]);
+console.log(favSingers[3]);
+console.log(favNumbers[1]);
+console.log(mixedArr[3]);
